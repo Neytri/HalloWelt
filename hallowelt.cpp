e9252df02857c64b50eb334a80a868e6b24ba838
Neytri <subbyberlin@googlemail.com>
2017-08-29 14:57:16 +0200

    insert spase as tab

--- hallowelt.cpp
+++ hallowelt.cpp
@@ -1,7 +1,7 @@
-#include <iostream>														// Ein- und Ausgabebibliothek
+#include <iostream>                                                   // Ein- und Ausgabebibliothek
 
-int main(){																// Hauptfunktion
-	std::cout << "Hello World!\nIch lerne jetzt \"C++\"." << std::endl; // Ausgabe in 2 Zeilen
+int main(){                                                           // Hauptfunktion
+  std::cout << "Hello World!\nIch lerne jetzt \"C++\"." << std::endl; // Ausgabe in 2 Zeilen
 
-	return 0;															// Optionale Rückgabe an das Betriebssystem
+  return 0;                                                           // Optionale Rückgabe an das Betriebssystem
 }
