df7a4817c8ebfe5b31468b3010f2762d6e201f08
Neytri <subbyberlin@googlemail.com>
2017-08-29 08:35:34 +0200

    Das wohl berühmteste Programm

---
+++ hallowelt.cpp
@@ -0,0 +1,7 @@
+#include <iostream>														// Ein- und Ausgabebibliothek
+
+int main(){																// Hauptfunktion
+	std::cout << "Hello World!\nIch lerne jetzt \"C++\"." << std::endl; // Ausgabe in 2 Zeilen
+
+	return 0;															// Optionale Rückgabe an das Betriebssystem
+}
